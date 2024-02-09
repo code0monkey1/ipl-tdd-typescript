c8aeab984f3fa0ee4fc83beed8bb3738fab86a96
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-09 09:16:42 +0000

    9 Feb 2024, 14:46

---
+++ test/scenarios/filters/fielded-after-winning-toss/index.spec.ts
@@ -0,0 +1,23 @@
+
+
+
+describe('fielded-after-winning-toss', () => {
+      
+
+    describe('isValid', () => {
+
+      
+      it.todo("win toss and choose to field")
+             
+      
+      it.todo('fielded but did not win toss')
+  
+  
+      it.todo('won toss but did not field')
+
+      
+    })
+    
+
+     
+})
