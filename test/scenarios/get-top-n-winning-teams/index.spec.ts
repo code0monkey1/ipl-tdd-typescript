fb03c57d860e0a9f480efe50249f10d21c04ca40
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-09 07:55:36 +0000

    9 Feb 2024, 13:25

--- test/scenarios/get-top-n-winning-teams/index.spec.ts
+++ test/scenarios/get-top-n-winning-teams/index.spec.ts
@@ -1,32 +1,29 @@
 describe('top-n-winning-teams', () => {
      
        
-    describe('Only 1 teams', () => {
-
-
-       it.todo("will only return the name of 1 teams")
-      
-    })
-
-
-    describe('Multiple teams', () => {
-      
-    })
-
-
-    describe(' when top-n is greater than the number of teams', () => {
-      
-
-    })
-
-
-    describe('when multiple the teams have same score', () => {
-
-
-
-      
-    })
+   describe('top 0 teams', () => {
+       
     
+   })
+
+
+   describe('top 1 team', () => {
+     
+   })
+
+
+   describe('top 2 teams', () => {
+     
+   })
+
+
+   describe('when multiple teams have same winning count', () => {
+     
+   })
+   
+   
+   
+   
     
     
     
