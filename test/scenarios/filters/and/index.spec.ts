88e8cd2c2736858b02f828ce516b1a1e4040d6ca
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-09 10:04:15 +0000

    9 Feb 2024, 15:34

--- test/scenarios/filters/and/index.spec.ts
+++ test/scenarios/filters/and/index.spec.ts
@@ -1,14 +1,45 @@
+import { IFilter } from "../../../../src/data/interfaces/filters"
+import { createFilterMatchByYear, createMatch } from "../match-played-in-year/helper"
 
+class And implements IFilter{
+
+  constructor(private filters:IFilter[]){}
+  isValid(): boolean {
+    throw new Error("Method not implemented.")
+  }
+
+}
 
 describe('filters', () => {
    
   describe('and', () => {
   
-      it.todo('meets all filter criteria')
+      it('meets all filter criteria',()=>{
+         
+          //arrange
+          
+          const match= createMatch({season:"2017"})
+
+          const filters=[createFilterMatchByYear(match,"2017")] 
+
+          const sut = new And(filters)
+
+          const expected = true
+
+          //act
+
+          const actual = sut.isValid()
+
+          //assert
+
+          expect(actual).toBe(expected)
+
+      })
 
       it.todo('does not meet all filter criteria')
     
   })
   
 })
+
 
