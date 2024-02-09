93a72f98c9d578a50f4152c51775e7cf9a5d1e5c
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-09 13:17:41 +0000

    9 Feb 2024, 18:47

--- src/controllers/q1.ts
+++ src/controllers/q1.ts
@@ -1,5 +1,8 @@
 import { IRepo } from "../data/interfaces";
 import { Match } from "../entities/Match";
+import { And } from "../scenarios/filters/and";
+import { MatchTeamFieldsAfterWinningToss } from "../scenarios/filters/fields-after-winning-toss";
+import { MatchPlayedInYear } from "../scenarios/filters/match-played-in-year";
 
 
 export class TopNTeamsThatElectedToFieldFirstAfterWinningTossInYear{
@@ -9,10 +12,23 @@ export class TopNTeamsThatElectedToFieldFirstAfterWinningTossInYear{
             private filterYear:string,
             private topN:number){
             
+              const matches = matchesRepo.data
+
             // filter matches by year , field first
+             const filteredMatches = matches.reduce((acc,match)=>{
+               
+                if(new And([new MatchPlayedInYear(match,filterYear),new MatchTeamFieldsAfterWinningToss(match)])){
+                   acc.push(match)
+                }
+                return acc
+              },[] as Match[])
+
+
 
             // get all unique team names that chose to field first
                 // which will an array of names
+              
+            
 
             // among the names , choose the ones that have top n wins
                 // which will be 
