c1d4790318da163160e56ce44dcd47a1036ff688
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-09 11:46:17 +0000

    9 Feb 2024, 17:16

--- test/scenarios/filters/compound/and/index.spec.ts
+++ test/scenarios/filters/compound/and/index.spec.ts
@@ -1,6 +1,7 @@
 
 import { createMatchTeamFieldsAfterWinningToss } from "../../fields-after-winning-toss/helper";
 import { createFilterMatchByYear, createMatch } from "../../filter-matches-by-year/helper";
+import { createOrFilter } from "../or/index.spec";
 import { createAnd } from './helper';
 
 describe('filters', () => {
@@ -76,19 +77,53 @@ describe('filters', () => {
     
       })
 
-      it('does meet 1 required criteria , and meets at least 1 of many optional criteria',()=>{
+      it('does meet 1 required filter , and meets at least 1 of many optional filters',()=>{
          
           //arrange
-          const match= createMatch({season:"2016",tossDecision:"bat"})
+          const match= createMatch({season:"2016",tossDecision:"field"})
+
+          const optionalCriteria = createOrFilter([ createFilterMatchByYear(match,"2017") ,  createFilterMatchByYear(match,"2016")])
 
           const filters=[
-            createFilterMatchByYear(match,"2017"),createMatchTeamFieldsAfterWinningToss(match)
+
+               createMatchTeamFieldsAfterWinningToss(match),
+               optionalCriteria
           ] 
           
           // const createOrFilter = new Create
           const sut = createAnd(filters)
 
           const expected = true
+
+          //act
+
+          const actual = sut.isValid()
+
+          //assert
+
+          expect(actual).toBe(expected)
+
+
+      })
+
+
+      it('meets all REQUIRED FILTERS , but NONE of the OPTIONAL FILTERS',()=>{
+         
+          //arrange
+          const match= createMatch({season:"2016",tossDecision:"field"})
+
+          const optionalCriteria = createOrFilter([ createFilterMatchByYear(match,"2017") ,  createFilterMatchByYear(match,"2015")])
+
+          const filters=[
+
+               createMatchTeamFieldsAfterWinningToss(match),
+               optionalCriteria
+          ] 
+          
+          // const createOrFilter = new Create
+          const sut = createAnd(filters)
+
+          const expected = false
 
           //act
 
