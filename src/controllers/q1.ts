615affbb26c7d7379596c0243fe378b8bfeb092e
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-09 13:05:17 +0000

    9 Feb 2024, 18:35

--- src/controllers/q1.ts
+++ src/controllers/q1.ts
@@ -5,8 +5,18 @@ import { Match } from "../entities/Match";
 class TopNTeamsThatElectedToFieldFirstAfterWinningTossInYear{
 
           constructor(private matchesRepo:IRepo<Match>,year:string,n:number){
+            
+            // filter matches by year , field first
+
+            // get all unique team names that chose to field first
+                // which will an array of names
+
+            // among the names , choose the ones that have top n wins
+                // which will be 
 
 
+            
+           
           }
   
 }
