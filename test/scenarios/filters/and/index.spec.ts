49596ee40d90aa7576009297e1531a6216a3f82e
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-09 09:58:13 +0000

    9 Feb 2024, 15:28

---
+++ test/scenarios/filters/and/index.spec.ts
@@ -0,0 +1,14 @@
+
+
+describe('filters', () => {
+   
+  describe('and', () => {
+  
+      it.todo('meets all filter criteria')
+
+      it.todo('does not meet all filter criteria')
+    
+  })
+  
+})
+
