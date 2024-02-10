0829fa8e165948fc8a612e1fa516a27f318221d1
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-10 15:18:09 +0000

    10 Feb 2024, 20:48

--- src/factories/index.ts
+++ src/factories/index.ts
@@ -1,27 +1,5 @@
-import { MapTeamWins, SortedTeamWins, TeamWinCountMap, TopNTeams, TopNWinningTeams } from "../controllers/q1";
+
 import { FilterMatchByTeamChoosingToField, FilterMatchByYear } from "../scenarios/filters/new";
-import { UniqueTossWinningTeams } from "../scenarios/unique-toss-winning-team-names";
-
-
-export const createTopWinningTeams=(topN:number)=>{
- 
-  const topNTeamWins =  new TopNTeams()
-  const teamWinCountMap = createTeamWinCountMap()
-  const sortedTeamWins = new SortedTeamWins()
-
-
-   return  new TopNWinningTeams(topN,teamWinCountMap,sortedTeamWins,topNTeamWins)
-
-}
-
-export const createTeamWinCountMap =()=>{
-
-  const teamWinCountMap = new TeamWinCountMap(
-  new UniqueTossWinningTeams(),
-  new MapTeamWins())
-
-  return teamWinCountMap
-}
 
 export const createMatchFilters=(year:string)=>{
 
