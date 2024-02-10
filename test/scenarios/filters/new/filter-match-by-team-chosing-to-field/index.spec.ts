8c97f376c783f7e68f586c6057a6ad3ff94d738d
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-10 08:16:44 +0000

    10 Feb 2024, 13:46

--- test/scenarios/filters/new/filter-match-by-team-chosing-to-field/index.spec.ts
+++ test/scenarios/filters/new/filter-match-by-team-chosing-to-field/index.spec.ts
@@ -1,1 +1,36 @@
-class
+
+import { Match } from "../../../../../src/entities/Match"
+import { IFilter } from "../../../../../src/scenarios/filters/new"
+
+class FilterMatchByTeamChoosingToField implements IFilter<Match>{
+  filter(arr: Match[]): Match[] {
+    throw new Error("Method not implemented.")
+  }
+      
+        
+}
+
+
+describe('filter-match-by-team-choosing-to-field', () => {
+
+
+  describe('1 team choosing to field after winning toss', () => {
+     
+    it('has 1 match who choose to field',()=>{
+        
+      //arrange
+
+      const sut = new FilterMatchByTeamChoosingToField()
+
+
+      //act
+
+      //assert
+
+    })
+
+    
+  })
+  
+  
+})
