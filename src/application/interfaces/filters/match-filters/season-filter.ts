632d343fc673a607ee711d5a3e54fc55eb66ee3f
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-19 16:28:50 +0000

    19 Feb 2024, 21:58

--- src/application/interfaces/filters/match-filters/season-filter.ts
+++ src/application/interfaces/filters/match-filters/season-filter.ts
@@ -2,7 +2,7 @@ import { Match } from '../../../../domain/entities/Match';
 import { IFilter } from '../filter';
 
 
-class YearFilter implements IFilter<Match> {
+class SeasonFilter implements IFilter<Match> {
   execute(data: Match[]): Match[] {
     throw new Error('Method not implemented.');
   }
--- test/application/filters/match-filters/year-filter.test.ts
+++ test/application/filters/match-filters/year-filter.test.ts
@@ -1,23 +1,29 @@
 import { IFilter } from '../../../../src/application/interfaces/filters/filter';
 import { Match } from '../../../../src/domain/entities/Match';
 
-class YearFilter implements IFilter<Match> {
+class SeasonFilter implements IFilter<Match> {
   execute(data: Match[]): Match[] {
     throw new Error('Method not implemented.');
   }
 
 }
 
-
-
 describe('year-filter', () => {
 
 
   describe('execute', () => {
-    
-    it('test',()=>{
-      expect(true).toBe(true)
+
+    describe('gets the matches with the years', () => {
+      
     })
+    
+    
+    it('filter year not present',()=>{
+      
+    })
+
+
+
   })
   
   
