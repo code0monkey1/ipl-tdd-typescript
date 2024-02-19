90f4cd6d3ac99be6ed5686b0559b911367fd5bce
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-19 08:02:57 +0000

    19 Feb 2024, 13:32

---
+++ src/domain/entities/Delivery.ts
@@ -0,0 +1,28 @@
+
+
+export type DeliveryProps={
+
+    matchId: number;
+    inning: number;
+    battingTeam: string;
+    bowlingTeam: string;
+    over: number;
+    ball: number;
+    batsman: string;
+    bowler: string;
+    wideRuns: number;
+    byeRuns: number;
+    legByeRuns: number;
+    noBallRuns: number;
+    penaltyRuns: number;
+    batsmanRuns: number;
+    extraRuns: number;
+    totalRuns: number;
+  
+}
+
+class Delivery {
+
+  constructor(private deliveryProps:DeliveryProps){}
+
+}
