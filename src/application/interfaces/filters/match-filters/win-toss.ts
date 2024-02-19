3bc6cb7923f0992bfaa312934f0e56465d7bf7f1
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-19 09:37:29 +0000

    19 Feb 2024, 15:07

--- src/application/interfaces/filters/match-filters/win-toss.ts
+++ src/application/interfaces/filters/match-filters/win-toss.ts
@@ -2,8 +2,14 @@ import { Match } from "../../../../domain/entities/Match";
 import { IFilter } from "../filter";
 
 export class WinTossFilter implements IFilter<Match>{
+
+  constructor(private matches:Match[]){}
+  
   execute(): Match[] {
-    throw new Error("Method not implemented.");
+    
+    const filteredMatches:Match[]=[]
+
+    return filteredMatches
   }
   
 }
