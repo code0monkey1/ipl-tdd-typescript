7cd2dee022668333fe56466e1c40a8a1c3fc9d4f
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-19 08:22:00 +0000

    19 Feb 2024, 13:52

--- src/domain/helpers/delivery-csv-helper.ts
+++ src/domain/helpers/delivery-csv-helper.ts
@@ -1,3 +1,10 @@
-export const isNotNumber = (argument: any): boolean =>
-  isNaN(Number(argument));
+export const isNumber = (argument: any): boolean =>!isNaN(Number(argument));
+
+export const isString =(argument:any):boolean=> argument instanceof String || typeof argument === 'string'
+
+export const csvEntryToDeliveryObj =(entry:string) =>{
+   
+
+
+}
 
