496dc90c800e908606bc966dbb5ed22e32830e13
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-19 08:51:45 +0000

    19 Feb 2024, 14:21

--- src/domain/entities/Match.ts
+++ src/domain/entities/Match.ts
@@ -8,7 +8,7 @@ export enum Result{
   TIE='tie'
 }
 
-type MatchDetails = {
+export type MatchProps = {
     matchId: number;
     season: number;
     city: string;
@@ -20,3 +20,30 @@ type MatchDetails = {
     result: Result
     winner: string;
 };
+
+
+export class Match {
+    private matchId: number;
+    private season: number;
+    private city: string;
+    private date: Date;
+    private team1: string;
+    private team2: string;
+    private tossWinner: string;
+    private tossDecision: TossDecision;
+    private result: Result;
+    private winner: string;
+
+    constructor(matchProps: MatchProps) {
+        this.matchId = matchProps.matchId;
+        this.season = matchProps.season;
+        this.city = matchProps.city;
+        this.date = matchProps.date;
+        this.team1 = matchProps.team1;
+        this.team2 = matchProps.team2;
+        this.tossWinner = matchProps.tossWinner;
+        this.tossDecision = matchProps.tossDecision;
+        this.result = matchProps.result;
+        this.winner = matchProps.winner;
+    }
+}
