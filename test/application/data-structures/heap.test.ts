95312a81761bc84360fe6db05276ace4b5e5ad32
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-21 14:01:13 +0000

    21 Feb 2024, 19:31

--- test/application/data-structures/heap.test.ts
+++ test/application/data-structures/heap.test.ts
@@ -8,25 +8,45 @@ console.log(numbers); //> [ 2, 3, 5, 7 ]
 // Pushes a new value to the heap
 Heap.heappush(numbers, 1);
 console.log(numbers); //> [ 1, 2, 5, 7, 3 ]
+
+class MyHeap{
+   
+     constructor(private numbers=[] as number []){
+       Heap.heapify(numbers)
+     }
+     heapPush(n:number){
+        Heap.heappush(this.numbers,n)
+     }
+
+     heapPop():number|undefined{
+      return Heap.heappop(this.numbers)
+     }
+
+     toString(){
+      Heap.toString()
+     }
+
+}
+
 describe('heap works well', () => {
 
   it('test',()=>{
      
     //arrange
       
-    const numbers = [2, 3, -1, 5];
+    const heap = new MyHeap([2, 3, -1, 5])
 
     // Changes the array elements order into a heap in-place
-    Heap.heapify(numbers);
+  
     console.log(numbers); //> [ 2, 3, 5, 7 ]
 
     // Pushes a new value to the heap
-    Heap.heappush(numbers, 1);
+    heap.heapPush( 1);
     console.log(numbers); //> [ 1, 2, 5, 7, 3 ]
 
-    Heap.heappush(numbers,100)
+    heap.heapPush(100)
 
-    console.log(Heap.heappop(numbers))
+    console.log(heap.heapPop())
 
   })
   
