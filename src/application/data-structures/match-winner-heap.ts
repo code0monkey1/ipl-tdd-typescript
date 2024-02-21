90c2eb50933816806e89945a213e47905cd56ba9
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-21 13:12:01 +0000

    21 Feb 2024, 18:42

--- src/application/data-structures/match-winner-heap.ts
+++ src/application/data-structures/match-winner-heap.ts
@@ -3,18 +3,14 @@ import { MyHeap } from "../interfaces/data-structures/heap";
 
 type TeamWins =Record<string,number>
 
-class MatchWinnerHeap extends Heap<TeamWins>{
-  
-  private arr: TeamWins[] = [] as TeamWins[]
+class MatchWinnerHeap extends Heap<TeamWins> implements MyHeap<TeamWins>{
+  heapPush(t: TeamWins): void {
+    throw new Error('Method not implemented.');
+  }
+  heapPop(): TeamWins | undefined {
+    throw new Error('Method not implemented.');
+  }
 
 
-  heapPush(e: TeamWins): void {
-    Heap.heappush(this.arr,e)
-  }
-  
-  heapPop(): TeamWins |undefined {
-    
-   return Heap.heappop(this.arr)
-  }
-  
+ 
 }
